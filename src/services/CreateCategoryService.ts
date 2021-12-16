de48b9224b727f3d38fb1548cab7dd0b0ff6e41e
Pablo <pabloguedesc@gmail.com>
2021-12-16 15:37:08 -0400

    Utilizando o Princípio da Substituição de Liskov

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
 
 interface IRequest {
   name: string;
@@ -6,12 +6,12 @@ interface IRequest {
 }
 
 class CreateCategoryService {
-  constructor(private categoriesRepository: CategoriesRepository) {}
+  constructor(private categoriesRepository: ICategoriesRepository) {}
 
   execute({ name, description }: IRequest): void {
     const categoryAlreadyExists = this.categoriesRepository.findByName(name);
 
-    if (categoryAlreadyExists) { 
+    if (categoryAlreadyExists) {
       throw new Error('Category Already exists');
     }
     this.categoriesRepository.create({ name, description });

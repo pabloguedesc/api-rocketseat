d599376810bb427c228a83505e04f99e6895830d
Pablo <pabloguedesc@gmail.com>
2021-12-29 13:19:40 -0400

    Instalando typeorm e reflect-metadata

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import swaggerUI from "swagger-ui-express";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
+import "./database";
 
 const app = express();
 

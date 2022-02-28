95b976cfd7614348bd1f514ea43014ac59f55bf1
Pablo <pabloguedesc@gmail.com>
2022-02-28 17:41:08 -0400

    refatoração de pastas

--- src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
@@ -1,7 +1,7 @@
 import { Repository, getRepository } from "typeorm";
-import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
-import { User } from "../../entities/User";
-import { IUsersRepository } from "../IUsersRepository";
+import { ICreateUserDTO } from "../../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../../repositories/IUsersRepository";
+import { User } from "../entities/User";
 
 class UsersRepository implements IUsersRepository {
   private repository: Repository<User>;

c69a8818b66014f8a00eddab76f584fe84c8d645
Pablo <pabloguedesc@gmail.com>
2022-05-04 13:47:10 -0400

    createCategoryController.spec.ts

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -2,7 +2,14 @@ import request from "supertest";
 import { app } from "@shared/infra/http/app";
 
 describe("Create Category Controller", () => {
-  it("test", async () => {
-    await request(app).get("/cars/available").expect(200);
+  it("Should be able to create a new category", async () => {
+    const response = await request(app).post("/categories").send({
+      name: "Category SuperTest Name",
+      description: "Category SuperTest Description",
+    });
+
+    expect(response.status).toBe(201);
   });
 });
+
+// parou na parte de criação do banco
